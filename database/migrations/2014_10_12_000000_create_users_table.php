3c2554a9a9ffd9d7d70ce692a670e338c08a5c7b
dipak <dipaktiwari89@gmail.com>
2023-04-11 10:30:22 +0530

    original code
    original code

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique()->nullable(false);
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable(false);
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
